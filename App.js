1b912987afbdb5b071c4acd6aa8ff75dbc79fe8c
Nwabufor John <johnzeus14@gmail.com>
2021-07-24 21:41:59 +0100

    added support for react-native Tailwinds

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tailwind('h-full bg-gray-50 flex justify-center items-center')}>
+      <Text style={tailwind('text-blue-600 font-body')}>Open up App.js to start working on your app!</Text>
+
+      <Text style={tailwind('text-red-600 font-body text-3xl tracking-wider')}>The second text!</Text>
+      <StatusBar style="" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
