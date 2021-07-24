7ba86514652d0dbb360f623ca54330ee658d2a28
Nwabufor John <johnzeus14@gmail.com>
2021-07-24 21:49:54 +0100

    added support for Tailwinds CSS

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import tailwind from 'tailwind-rn';
 
 export default function App() {
@@ -9,6 +9,27 @@ export default function App() {
       <Text style={tailwind('text-blue-600 font-body')}>Open up App.js to start working on your app!</Text>
 
       <Text style={tailwind('text-red-600 font-body text-3xl tracking-wider')}>The second text!</Text>
+
+      <View style={tailwind('mt-24 flex')}>
+
+      <Button
+        onPress={() => alert('Right button pressed')}
+        title="Learn More"
+        style={tailwind('h-full bg-green-600 font-body py-3 px-4')}
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+  <Button
+        onPress={() => Alert.alert('Right button pressed')}
+        title="Learn More"
+        style={tailwind('h-full bg-green-600 font-body py-3 px-4')}
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+
+      </View>
+
+
       <StatusBar style="" />
     </View>
   );
